17014a28b8cb436e46b8bab1ce6d1f3c3fb3016e
Heilika <143094865+Heilika@users.noreply.github.com>
2023-11-07 13:12:26 +0200

    Esimene

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import Luuletus1 from './Luuletus1';
+import Luuletus2 from './Luuletus2';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const eesNimi = 'Heilika';
+  const pereNimi = 'Ahven';
+  const number = 10;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-link">
+          Minu nimi on {eesNimi} {pereNimi} {number + 100}
+        </div>
+        <Luuletus1 />
+       <div>----------------------------</div>
+        <Luuletus2 />
       </header>
     </div>
   );
---
+++ src/Luuletus1.js
@@ -0,0 +1,27 @@
+function Luuletus1 () {
+    return (
+        <div>
+            <p>
+            MU JUUKSED KASVAVAD LÄBI LAE<br/>
+            Ja haarduvad pilvede külge<br/>
+            </p>
+            <p>
+            Ja mis nendest saab<br/>
+            seda mina ei näe<br/>
+            ja küsida ka ma ei julge<br/>
+            </p>
+            <p>
+            Sest sealpool on valgus<br/>
+            ja heledad näod<br/>
+            hõljumas pilvesid pidi<br/>
+            </p>
+            <p>
+            ja siinpool on<br/>
+            mardikad tõugud ja teod<br/>
+            ja muld minu põskede ligi
+            </p>
+        </div>        
+    )
+}
+
+export default Luuletus1;
